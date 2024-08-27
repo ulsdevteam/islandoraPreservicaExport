c48c87c8d775651e71a4056e80e615710b1c27e8
Emily Vaiz <emv38@pitt.edu>
2024-08-27 18:16:39 -0400

    adding parameter checks

--- bagit-pax/pitt_pax_v2.py
+++ bagit-pax/pitt_pax_v2.py
@@ -19,7 +19,9 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 if len(sys.argv) != 3:
     #no parameters given so script with prompt during its run
+    print("running interactive script using pitt.ini")
 else:
+    print("running automatic script using pittautomated.ini")
     first_step = sys.argv(1)
     c_f_input = sys.argv(2)
     send_to_s3 = sys.argv(3) 
