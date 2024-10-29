93ee15b3371a3e52cccbd5cd25accd3a0f7506dc
Emily Vaiz <emv38@pitt.edu>
2024-10-29 12:09:29 -0400

    using case statements for archive automation

--- archiveAutomation.sh
+++ archiveAutomation.sh
@@ -121,7 +121,7 @@ check_worker_status() {
     worker_directory="/mounts/transient/pa-gmworker-0$1/bags/"
     STATUS=$(python3 $CSV_SCRIPT "workerStatus" "$1")
     #echo "status: $STATUS"
-    if [ "$STATUS" = "Ready" ]; then
+    if [ "$STATUS" = "Ready" ] || [ "$STATUS" = "archiveERROR" ]; then
         if [ "$(ls $worker_directory | grep DC.xml)" ]; then
             return 0;
         else
@@ -129,19 +129,16 @@ check_worker_status() {
         fi
     elif [ "$STATUS" = "NEEDNEWCOLLECTION" ]; then
         echo "status of worker $1 is None meaning it's ready for a new collection"
-        return 2;
-        # COLLECTION=$(python3 $CSV_SCRIPT "workerFind" "$1")
-        # if [ "COLLECTION" = "None" ]; then   
-        #     return 2;
-        # fi
-        # echo "need to start export from gmworker-0$1" 
-        # return 1;
+        return 3;
     elif [ "$STATUS" = "ERROR" ]; then
-        log_error "ERROR found in worker $1"
-        return 1;
+        log_error "ERROR found in worker $1 server"
+        return 4;
+    elif [ "$STATUS" = "removing" ]; then
+        echo "removing collections"
+        return 2;
     else
         log_error "other error found within worker $1 with status: $STATUS"
-        return 1;
+        return 4;
     fi 
 
 }
@@ -216,13 +213,15 @@ start_transfer() {
 }
 
 run_automated_pittPax() {
-    
-    
-    python3 "$PITT_PAX_V2_SCRIPT" "1" "ALL" "1"
+    COLLECTION=$1
+
+    python3 $CSV_SCRIPT "$COLLECTION" "status" "ppax"
+    output=$(python3 "$PITT_PAX_V2_SCRIPT" "1" "ALL" "1" 2>&1)
+    #python3 "$PITT_PAX_V2_SCRIPT" "1" "ALL" "1"
     if [ $? -ne 0 ]; then
-        log_error_exit "unable to run the automated pitt pax script - proceed with interactive"
-    fi 
-    
+        log_error "unable to run the automated pitt pax script, see output: $output"
+    fi  
+
 }
 
 #remove all previous collections from shared mount of gmworkers, and pittpax location - 
@@ -232,14 +231,14 @@ remove_old_collections() {
 
     output=$(rm -rf /mounts/transient/pittpax/Master/Final/* 2>&1)
     if [ $? -ne 0 ]; then
-        log_error_exit "$output"
+        log_error "$output"
     fi
 
     #echo "removing from: /mounts/transient/$1/bags/*"
 
     output=$(rm -rf /mounts/transient/$1/bags/* 2>&1)
     if [ $? -ne 0 ]; then
-        log_error_exit "$output"
+        log_error "$output"
     fi
 
 }
@@ -286,13 +285,13 @@ do
 
     check_worker_status "$i"
     worker_status=$?
+    COLLECTION=$(get_collection_number "$i")
 
-    # 5 - archiveError issue that needs to be manually handled check the logs..
     # 0 - Ready to start from the beginning
-    # 1 - transfer process started/yet to start 
-    # 2 - pitt pax script running/yet to run
-    # 3 - removing old collections
-    # 4 - assigning to a new collection
+    # 1 - automated pittpax script
+    # 2 - removing old collections
+    # 3 - assigning new collections
+    # * - unkown error code
     
     case $worker_status in 
     0)
@@ -304,73 +303,104 @@ do
         echo "starting transfer now"
         update_log "$COLLECTION" "$i" "archive03 transfer starting"
 
+        python3 $CSV_SCRIPT "$COLLECTION" "status" "transferring"
         start_transfer "$i" "$COLLECTION"
 
-        python3 $CSV_SCRIPT "$COLLECTION" "status" "In Progress"
+        python3 $CSV_SCRIPT "$COLLECTION" "status" "transfered"
         if [ $? -ne 0 ]; then
             log_error_exit "Error csvUpdate.py didn't update status successfully for collection $COLLECTION in pa-gmworker-0$i"
         fi      
         
+        # #separate method for python script to run
+        # update_log "$COLLECTION" "$i" "starting pitt pax script"
+        # run_automated_pittPax
+        # if [ $? -ne 0 ]; then 
+        #     log_error_exit "error running automated pittpax script for collection $COLLECTION in pa-gmworker-0$i"
+        # fi
+        # update_log "$COLLECTION" "$i" "pitt pax script completed"
+
+
+        # update_log "$COLLECTION" "$i" "removing collections"
+        # remove_old_collections "pa-gmworker-0$i"
+        # if [ $? -ne 0 ]; then 
+        #     log_error_exit "error removing collections for collection $COLLECTION in pa-gmworker-0$i"
+        # fi
+        
+
+        # update_log "$COLLECTION" "$i" "$COLLECTION exported from pa-gmworker-0$i updating csv..."
+        # python3 $CSV_SCRIPT "$COLLECTION" "status" "Complete"
+        # if [ $? -ne 0 ]; then
+        #     log_error_exit "Error csvUpdate.py didn't update status to Complete successfully for collection $COLLECTION in pa-gmworker-0$i"
+        # fi   
+
+        # #assign a new collection
+        # assign_worker "$i"
+        # if [ $? -ne 0 ]; then 
+        #     log_error_exit "error assigning new collection for pa-gmworker-0$i"
+        # fi
+
+        # #add to a temp log
+        # add_to_temp "$COLLECTION" "$i"
+        # if [ $? -ne 0 ]; then 
+        #     log_error_exit "error adding to temp log for pa-gmworker-0$i"
+        # fi
+
+        ;&
+    1) 
+        echo "automated pittpax starting"
+
         #separate method for python script to run
         update_log "$COLLECTION" "$i" "starting pitt pax script"
-        run_automated_pittPax
+        run_automated_pittPax "$COLLECTION"
         if [ $? -ne 0 ]; then 
+            python3 $CSV_SCRIPT "$COLLECTION" "status" "archiveERROR"
             log_error_exit "error running automated pittpax script for collection $COLLECTION in pa-gmworker-0$i"
         fi
         update_log "$COLLECTION" "$i" "pitt pax script completed"
 
+        ;&
+    2)
+        echo "removing old collections"
 
         update_log "$COLLECTION" "$i" "removing collections"
+        python3 $CSV_SCRIPT "$COLLECTION" "status" "removing"
         remove_old_collections "pa-gmworker-0$i"
-        if [ $? -ne 0 ]; then 
+        if [ $? -ne 0 ]; then
             log_error_exit "error removing collections for collection $COLLECTION in pa-gmworker-0$i"
         fi
-        
 
         update_log "$COLLECTION" "$i" "$COLLECTION exported from pa-gmworker-0$i updating csv..."
         python3 $CSV_SCRIPT "$COLLECTION" "status" "Complete"
         if [ $? -ne 0 ]; then
             log_error_exit "Error csvUpdate.py didn't update status to Complete successfully for collection $COLLECTION in pa-gmworker-0$i"
-        fi   
-
-        #assign a new collection
+        fi  
+        
+        ;&
+    3)
+        echo "assign new collection to worker"
         assign_worker "$i"
         if [ $? -ne 0 ]; then 
             log_error_exit "error assigning new collection for pa-gmworker-0$i"
         fi
 
-        #add to a temp log
-        add_to_temp "$COLLECTION" "$i"
-        if [ $? -ne 0 ]; then 
-            log_error_exit "error adding to temp log for pa-gmworker-0$i"
-        fi
-
-        ;;
-    1) 
-        echo "pa-gmworker-0$i still running transfer"
-        ;;
-    2)
-        echo "pa-gmworker-0$i ready for new collection"
-        assign_worker "$i"
         ;;
     *)
         log_error_exit "unknown status code for worker $i: $worker_status"
         ;;
     esac
 
-
 done
 
-if [ -f "$TEMP_FILE" ]; then
-    #has items in it that need to be mailed
-    DATE=$(date)
-    #echo "$HOSTNAME run completed at $DATE" | mutt -a "$TEMP_FILE" -s 'Archive transfers complete - wait for OPEX' emv38@pitt.edu
+# if [ -f "$TEMP_FILE" ]; then
+#     #has items in it that need to be mailed
+#     DATE=$(date)
+#     #echo "$HOSTNAME run completed at $DATE" | mutt -a "$TEMP_FILE" -s 'Archive transfers complete - wait for OPEX' emv38@pitt.edu
 
-    # rm -f "$TEMP_FILE"
-    # if [ $? -ne 0 ]; then 
-    #     log_error_exit "Error removing $TEMP_FILE - must manually do so before next cron"
-    # fi
+#     # rm -f "$TEMP_FILE"
+#     # if [ $? -ne 0 ]; then 
+#     #     log_error_exit "Error removing $TEMP_FILE - must manually do so before next cron"
+#     # fi
 
-fi 
+# fi 
 
 #-----------------------------------------------------------------------------------------
